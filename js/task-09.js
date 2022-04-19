d3da2d74e925afec43c66dc1d246eece8142d23e
Cтас <90937465+sfirsovdev@users.noreply.github.com>
2022-04-20 01:18:30 +0300

    Финал

--- js/task-09.js
+++ js/task-09.js
@@ -6,8 +6,7 @@
   <p>Background color: <span class="color">-</span></p>
   <button type="button" class="change-color">Change color</button>
 </div>
-Для генерации случайного цвета используй 
-функцию getRandomHexColor.
+Для генерации случайного цвета используй функцию getRandomHexColor.
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
